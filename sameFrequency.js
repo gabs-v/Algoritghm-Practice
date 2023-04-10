0fe5c732df22fc97d37b60de5af6683de2ef6a6c
gabs-v <gabyvazquez8297@gmail.com>
2023-04-09 18:59:11 -0700

    cleanup

---
+++ sameFrequency.js
@@ -0,0 +1,14 @@
+//PROBLEM: Frequency Counter - sameFrequency
+// Write a function called sameFrequency. Given two positive integers, 
+//find out if the two numbers have the same frequency of digits.
+// Your solution MUST have the following complexities:
+// Time: O(N)
+// Sample Input:
+// sameFrequency(182,281) // true
+// sameFrequency(34,14) // false
+// sameFrequency(3589578, 5879385) // true
+// sameFrequency(22,222) // false
+
+//Restate the problem - write a function  that compares two positive integers, and sees if they are the same frequency 
+//Breaking it down: 
+// write a function named sameFrequency  -- 
